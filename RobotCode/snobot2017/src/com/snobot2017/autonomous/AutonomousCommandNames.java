f39d99f8644a31c2801b40fd1f14f69d02f2c596
Owner <Owner@Nora>
2017-01-19 19:30:03 -0500

    Added StupidDriveStraight command

--- RobotCode/snobot2017/src/com/snobot2017/autonomous/AutonomousCommandNames.java
+++ RobotCode/snobot2017/src/com/snobot2017/autonomous/AutonomousCommandNames.java
@@ -5,5 +5,6 @@ public class AutonomousCommandNames
 
     public static final String sPARALLEL_COMMAND = "Parallel";
     public static final String sWAIT_COMMAND = "Wait";
+    public static final String sSTUPID_DRIVE_STRAIGHT_COMMAND = "StupidDriveStraight";
 
 }
