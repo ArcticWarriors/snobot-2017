aeff6fab909c4328eab982a8cee95d0432df00c5
Nora <mcconnellnora@gmail.com>
2017-01-20 19:39:12 -0500

    Added GearBoss package with interface and skeletal class.

---
+++ RobotCode/snobot2017/src/com/snobot2017/gearboss/IGearBoss.java
@@ -0,0 +1,26 @@
+package com.snobot2017.gearboss;
+
+import com.snobot.lib.ISubsystem;
+/**
+ * Interface controls gear position
+ * @author Owner
+ *
+ */
+public interface IGearBoss extends ISubsystem
+{
+   /**
+    * Moves gear to its high position. This is to get the gear onto the peg.
+    */
+    void moveGearHigh();
+    /**
+     * This is to get the Gear Boss down and away from the gear. This is the second 
+     * part of the process of putting the gear on the peg.
+     */
+    void moveGearLow();
+    /**
+     * This returns the position of the gear bucket. 
+     * @return true for gear high and false for gear low
+     */
+    boolean getGearHeight();
+}
+
