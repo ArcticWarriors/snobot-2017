2cfa7e0017f5b0bc1006f7ab2c40a30b2de10836
PJ Reiniger <pj.reiniger@gmail.com>
2017-01-29 01:28:12 -0500

    Seperating rope and peg grip algorithms.  Front does peg, rear does rope

--- Vision/VisionApp/app/src/main/java/com/snobot/vision_app/app2017/SnobotVisionGLActivity.java
+++ Vision/VisionApp/app/src/main/java/com/snobot/vision_app/app2017/SnobotVisionGLActivity.java
@@ -87,6 +87,7 @@ public class SnobotVisionGLActivity extends Activity implements VisionRobotConne
 
     @Override
     public void useCamera(int aCameraId) {
+        mAlgorithm.setCameraDirection(aCameraId);
         mView.setCameraIndex(aCameraId);
     }
 
--- Vision/VisionApp/app/src/main/java/com/snobot/vision_app/app2017/java_algorithm/GripPegAlgorithm.java
+++ Vision/VisionApp/app/src/main/java/com/snobot/vision_app/app2017/java_algorithm/GripPegAlgorithm.java
@@ -18,7 +18,7 @@ import java.util.List;
  * Created by preiniger on 1/24/2017.
  */
 
-public class VisionGripAlgorithm {
+public class GripPegAlgorithm {
 
     //Outputs
     private Mat hslThresholdOutput = new Mat();
