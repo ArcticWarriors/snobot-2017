44d22c4dcc9d983e30d8032c0add165b836e4d40
PJ Reiniger <pj.reiniger@gmail.com>
2017-02-01 21:56:57 -0500

    Updating the algorithm to calculate angle... if it works

--- Testbed/VisionTestSuite/src/com/snobot/vision_app/app2017/java_algorithm/VisionAlgorithm2.java
+++ Testbed/VisionTestSuite/src/com/snobot/vision_app/app2017/java_algorithm/VisionAlgorithm2.java
@@ -4,12 +4,16 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
@@ -25,6 +29,17 @@ public class VisionAlgorithm2 implements IVisionAlgorithm
 
     private static final double sTARGET_WIDTH = 2;
     private static final double sTARGET_HEIGHT = 5;
+
+    private static final Point sCENTER_LINE_START = new Point(sIMAGE_WIDTH / 2, 0);
+    private static final Point sCENTER_LINE_END = new Point(sIMAGE_WIDTH / 2, sIMAGE_HEIGHT);
+
+    private static final Scalar sCENTER_LINE_COLOR = new Scalar(0, 255, 0);
+
+    private static final List<Scalar> sCONTOUR_COLORS = Arrays.asList(new Scalar[] { 
+            new Scalar(0,   0,   0), 
+            new Scalar(255, 0,   0), 
+            new Scalar(0,   255, 255) 
+    });
 
     protected List<ProcessedImageListener> mUpdateListeners;
     protected BufferedImage mCurrentImage;
@@ -64,18 +79,21 @@ public class VisionAlgorithm2 implements IVisionAlgorithm
     {
         mPipeline.process(originalImage);
 
-        // Mat postThreshold = new Mat();
-        // originalImage.copyTo(postThreshold);
-        // Core.polylines(postThreshold, pipeline.filterContoursOutput(), true,
-        // new Scalar(255, 0, 0));
+//        Mat displayImage = new Mat();
+//        Imgproc.cvtColor(mPipeline.hslThresholdOutput(), displayImage, Imgproc.COLOR_GRAY2BGR);
 
-        Mat displayImage = mPipeline.hslThresholdOutput();
+        Mat displayImage = new Mat();
+        originalImage.copyTo(displayImage);
+
+        Core.line(displayImage, sCENTER_LINE_START, sCENTER_LINE_END, sCENTER_LINE_COLOR, 1);
 
         DecimalFormat df = new DecimalFormat("0.0000");
 
         ArrayList<MatOfPoint> contours = mPipeline.filterContoursOutput();
         for (int i = 0; i < contours.size(); ++i)
         {
+            Imgproc.drawContours(displayImage, contours, i, sCONTOUR_COLORS.get(i % sCONTOUR_COLORS.size()), 3);
+
             MatOfPoint contour = contours.get(i);
             Rect rect = Imgproc.boundingRect(contour);
             double aspectRatio = rect.width * 1.0 / rect.height;
@@ -83,12 +101,21 @@ public class VisionAlgorithm2 implements IVisionAlgorithm
             double distanceFromHorz = (sTARGET_WIDTH * sIMAGE_WIDTH) / (2 * rect.width * Math.tan(sHORIZONTAL_FOV_ANGLE));
             double distanceFromVert = (sTARGET_HEIGHT * sIMAGE_HEIGHT) / (2 * rect.height * Math.tan(sVERTICAL_FOV_ANGLE));
 
+            // TODO I don't know if this will work...
+
+            // Calculate the angle by calculating how far off the center the
+            // bounding box is. Assume that the ratio of pixels to angle is
+            // linear, meaning that it is off by the FOV
+            double x_centroid = rect.x + rect.width / 2;
+            double distanceFromCenterPixel = x_centroid - sIMAGE_WIDTH / 2;
+            double percentOffCenter = distanceFromCenterPixel / sIMAGE_WIDTH * 100;
+            double yawAngle = percentOffCenter * sHORIZONTAL_FOV_ANGLE;
+
             System.out.println("Contour " + i);
-            System.out.println("  Aspect Ratio: " + df.format(aspectRatio));
-            System.out.println("  Horizontal Calculations: Dist=" + df.format(distanceFromHorz));
-            System.out.println("  Vertical Calculations  : Dist=" + df.format(distanceFromVert));
-            // System.out.println(distanceFromHorz + ", " + distanceFromVert);
-            // System.out.println(aspectRatio + "," + contour + ", " + rect);
+            System.out.println("  Aspect Ratio         : " + df.format(aspectRatio));
+            System.out.println("  Angle                : " + df.format(yawAngle));
+            System.out.println("  Distance From Horz.  : Dist=" + df.format(distanceFromHorz));
+            System.out.println("  Distance From Vert.  : Dist=" + df.format(distanceFromVert));
         }
 
         for (ProcessedImageListener listener : mUpdateListeners)
