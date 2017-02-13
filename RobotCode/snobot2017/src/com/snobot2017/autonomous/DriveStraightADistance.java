350902439b35b5d70f3338e191170723b21c04ea
PJ Reiniger <pj.reiniger@gmail.com>
2017-02-12 20:27:55 -0500

    Fixing auto modes

--- RobotCode/snobot2017/src/com/snobot2017/autonomous/DriveStraightADistance.java
+++ RobotCode/snobot2017/src/com/snobot2017/autonomous/DriveStraightADistance.java
@@ -6,19 +6,15 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveStraightADistance extends Command
 {
-    private double mDistance;
-    private double mSpeed;
-    private double mStartDistance;
     private boolean mFinished;
     private ISnobotActor mSnobotActor;
 
     public DriveStraightADistance(double aDistance, double aSpeed, ISnobotActor aSnobotActor)
     {
         mSnobotActor = aSnobotActor;
-        mDistance = aDistance;
-        mSpeed = aSpeed;
         mFinished = false;
-        mSnobotActor.setGoal(mDistance, aSpeed);
+
+        mSnobotActor.setGoal(aDistance, aSpeed);
     }
 
     @Override
--- RobotCode/snobot2017/src/com/snobot2017/autonomous/TurnWithDegrees.java
+++ RobotCode/snobot2017/src/com/snobot2017/autonomous/TurnWithDegrees.java
@@ -1,10 +1,6 @@
 package com.snobot2017.autonomous;
 
-import com.snobot.lib.InDeadbandHelper;
 import com.snobot2017.SnobotActor.ISnobotActor;
-import com.snobot2017.SnobotActor.SnobotActor;
-import com.snobot2017.drivetrain.IDriveTrain;
-import com.snobot2017.positioner.IPositioner;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -16,13 +12,9 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class TurnWithDegrees extends Command
 {
-    private SnobotActor mSnobotActor;
+    private ISnobotActor mSnobotActor;
     private double mSpeed;
     private double mTurnAngle;
-    private IDriveTrain mDriveTrain;
-    private IPositioner mPositioner;
-    private double mTurnMeasure;
-    private InDeadbandHelper mInDeadbandHelper;
     private boolean mFinished;
 
     /**
@@ -35,6 +27,7 @@ public class TurnWithDegrees extends Command
      */
     public TurnWithDegrees(double aSpeed, double aTurnAngle, ISnobotActor aSnobotActor)
     {
+        mSnobotActor = aSnobotActor;
         mTurnAngle = aTurnAngle;
         mSpeed = aSpeed;
         mFinished = false;
@@ -44,31 +37,6 @@ public class TurnWithDegrees extends Command
     protected void execute()
     {
         mFinished = mSnobotActor.turnToAngle(mTurnAngle, mSpeed);
-        mTurnMeasure = (mTurnAngle - mPositioner.getOrientationDegrees()) % 360;
-        if ((mTurnMeasure) < 0)
-        {
-            mTurnMeasure = mTurnMeasure + 360;
-        }
-        if (mTurnMeasure <= 180)
-        {
-            mDriveTrain.setLeftRightSpeed(mSpeed, -mSpeed);
-        }
-        else
-        {
-            mDriveTrain.setLeftRightSpeed(-mSpeed, mSpeed);
-        }
-
-        if (mInDeadbandHelper.isFinished(Math.abs(mTurnAngle - mPositioner.getOrientationDegrees()) < 5))
-        {
-            mDriveTrain.setLeftRightSpeed(0, 0);
-            mFinished = true;
-        }
-        else
-        {
-            mFinished = false;
-        }
-        
-        //System.out.println("TurnWithDegrees " + mTurnAngle + " " + mPositioner.getOrientationDegrees() + " " + mTurnMeasure);
     }
 
     @Override
