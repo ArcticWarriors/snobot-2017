19226dc5ecacb27e07b0f4433fb6b68115518c79
Ayush Goel <ag.goelayush@gmail.com>
2017-02-17 17:38:43 -0500

    Testing real robt

--- RobotCode/snobot2017/src/com/snobot2017/joystick/SnobotDriveXbaxJoystick.java
+++ RobotCode/snobot2017/src/com/snobot2017/joystick/SnobotDriveXbaxJoystick.java
@@ -1,6 +1,7 @@
 package com.snobot2017.joystick;
 
 import com.snobot.lib.Logger;
+import com.snobot.lib.Utilities;
 import com.snobot.lib.ui.XboxButtonMap;
 import com.snobot2017.SmartDashBoardNames;
 
@@ -9,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SnobotDriveXbaxJoystick implements IDriverJoystick
 {
+	private static final double sJOYSTICK_DEADBAND = .032;
     private Joystick mJoystick;
     private double mLeftSpeed;
     private double mRightSpeed;
@@ -31,8 +33,19 @@ public class SnobotDriveXbaxJoystick implements IDriverJoystick
     @Override
     public void update()
     {
-        mLeftSpeed = -mJoystick.getRawAxis(XboxButtonMap.LEFT_Y_AXIS);
-        mRightSpeed = -mJoystick.getRawAxis(XboxButtonMap.RIGHT_Y_AXIS);
+    	double leftJoystick = -Utilities.stopInDeadband(mJoystick.getRawAxis(XboxButtonMap.LEFT_Y_AXIS), sJOYSTICK_DEADBAND);
+        double rightJoystick = -Utilities.stopInDeadband(mJoystick.getRawAxis(XboxButtonMap.RIGHT_Y_AXIS), sJOYSTICK_DEADBAND);
+        
+
+        double leftNeg = Math.signum(leftJoystick);
+        double rightNeg = Math.signum(rightJoystick);
+        
+
+//        mRightSpeed = rightJoystick * rightJoystick * rightNeg;
+//        mLeftSpeed = leftJoystick * leftJoystick * leftNeg;
+
+        mRightSpeed = rightJoystick;
+        mLeftSpeed = leftJoystick;
     }
 
     @Override
