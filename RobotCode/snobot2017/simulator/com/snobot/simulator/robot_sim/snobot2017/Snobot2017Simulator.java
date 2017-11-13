4a3093b0502394dbfd188c922d2681878c564b6f
pjreiniger <pj.reiniger@gmail.com>
2017-11-13 06:38:42 -0500

    Cleaning up

--- RobotCode/snobot2017/simulator/com/snobot/simulator/robot_sim/snobot2017/Snobot2017Simulator.java
+++ RobotCode/snobot2017/simulator/com/snobot/simulator/robot_sim/snobot2017/Snobot2017Simulator.java
@@ -1,7 +1,5 @@
 package com.snobot.simulator.robot_sim.snobot2017;
 
-import java.util.Map;
-
 import com.snobot.simulator.ASimulator;
 import com.snobot.simulator.config.SimulatorConfigReader;
 import com.snobot2017.PortMappings2017;
@@ -26,33 +24,33 @@ public class Snobot2017Simulator extends ASimulator
             return super.loadConfig(configFile);
         }
 
-        @Override
-        protected void parseConfig(Object aConfig)
-        {
-            super.parseConfig(aConfig);
-
-            @SuppressWarnings("unchecked")
-            Map<String, Object> config = (Map<String, Object>) aConfig;
-
-            boolean simulateCamera = true;
-
-            if (config.containsKey("simulate_camera"))
-            {
-                simulateCamera = (Boolean) config.get("simulate_camera");
-            }
-
-            if (simulateCamera)
-            {
-                CameraSimulator cameraSimulator = new CameraSimulator();
-                // mSimulatorComponenets.add(cameraSimulator);
-            }
-        }
+//        @Override
+//        protected void parseConfig(Object aConfig)
+//        {
+//            super.parseConfig(aConfig);
+//
+//            @SuppressWarnings("unchecked")
+//            Map<String, Object> config = (Map<String, Object>) aConfig;
+//
+//            boolean simulateCamera = true;
+//
+//            if (config.containsKey("simulate_camera"))
+//            {
+//                simulateCamera = (Boolean) config.get("simulate_camera");
+//            }
+//
+//            if (simulateCamera)
+//            {
+//                CameraSimulator cameraSimulator = new CameraSimulator();
+//                // mSimulatorComponenets.add(cameraSimulator);
+//            }
+//        }
     }
-
-    @Override
-    protected void createSimulatorComponents(String aConfigFile)
-    {
-        new Snobot2017ConfigReader().loadConfig(aConfigFile);
-    }
+//
+//    @Override
+//    protected void createSimulatorComponents(String aConfigFile)
+//    {
+//        new Snobot2017ConfigReader().loadConfig(aConfigFile);
+//    }
 
 }
--- Simulator/2017Simulator/open_smart_dashboard.bat
+++
@@ -1,2 +0,0 @@
-cd %userprofile%\wpilib\tools
-java -jar SmartDashboard.jar ip 127.0.0.1
